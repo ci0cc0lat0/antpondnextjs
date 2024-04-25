e741e4bfabd4e281476c704b5be36949ee7893dd
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-04-25 17:22:34 -0500

    Fixed url

--- pages/records.js
+++ pages/records.js
@@ -4,19 +4,28 @@ import Link from 'next/link'
 import AudioComp from '../components/AudioComp';
 import {recordings} from '../components/recordings'
 
-/* export async function getStaticProps(){
-  const res  = await fetch("http://192.168.1.253:1337/api/records?sort=createdAt:desc&populate=*")
-  const post = await res.json()
-
-  return{
-    props:{
-      post,
+ export async function getStaticProps(){
+  try{
+    const res  = await fetch("http://162.207.101.36:1337/api/records?sort=createdAt:desc&populate=*")
+    if(!res.ok) throw new Error("Our bad!")
+    const post = await res.json()
+    return{
+      props:{
+        post,
+      }
     }
+  }catch(error){
+    console.error('Fetch error:',error)
+    throw error
   }
+  
 
-} */
+  
 
-export default function records() {
+} 
+
+export default function records({post}) {
+  console.log(post)
   return (
     <>
         <div className="title-flip">
@@ -25,12 +34,12 @@ export default function records() {
         </Link>
         <hr></hr>
         </div>
-        {recordings.map(record => {
+        {post.data.map(record => {
             return(
                 <AudioComp
-                audio_name={record.name}
-                audio_src={record.src}
-                audio_caption={record.caption}
+                audio_name={record.attributes.Title}
+                audio_src={record.attributes.source.data.attributes.name}
+                audio_caption={record.attributes.Caption}
             />
             )
           
