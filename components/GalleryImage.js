97e1d65025ea22a297c329606c2aa68350918b33
ci0cc0lat0 <anthonymciocco@gmail.com>
2024-06-24 08:25:07 -0500

    encodeURI

--- components/GalleryImage.js
+++ components/GalleryImage.js
@@ -3,11 +3,12 @@ import Link from 'next/link'
 
 
 export default function GalleryImage({name,tag}) {
+  const encoded_URL = encodeURI(name)
   return (
     <>
         <div className='gallery-images'>
-            <Link href={`/${name}`} target="_blank" rel="noreferrer">
-            <img loading="lazy" src={`/${name}`} className={`each-img ${tag}` } />
+            <Link href={`/${encoded_URL}`} target="_blank" rel="noreferrer">
+            <img loading="lazy" src={`/${encoded_URL}`} className={`each-img ${tag}` } />
             </Link>
         </div>
     </>
